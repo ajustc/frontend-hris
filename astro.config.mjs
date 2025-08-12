205c270c0c885d408ff7602ad98cd3150a34efc0
Rio Justicio <riojusticiof@gmail.com>
2025-08-12 20:13:05 +0700

    Adjustment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      configFile: './tailwind.config.js',
-    }),
-    react()
-  ],
-  output: 'static',
-  build: {
-    assets: 'assets'
-  }
+	integrations: [
+		tailwind({
+			configFile: "./tailwind.config.js",
+		}),
+		react(),
+	],
+	output: "static",
+	build: {
+		assets: "assets",
+	},
 });
